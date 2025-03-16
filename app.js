727a073f7675caef66d726e1e90200e50d2e567d
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-03-17 00:34:37 +0700

    finish the project

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ function addTodo(){
   const todoText = todoInput.value.trim();
   
   if (todoText.length > 0) {
-    allTodo.push(todoText);
+    const todoObject = {
+      text: todoText,
+      completed: false
+    }
+
+    allTodo.push(todoObject);
     updateTodoList();
     saveTodos();
     todoInput.value = "";
@@ -30,9 +35,10 @@ function updateTodoList(){
   })
 }
 
-function createTodoItem(todotext, todoIndex){
+function createTodoItem(todo, todoIndex){
   const todoId = "todo-"+todoIndex;
   const todoLi = document.createElement('li');
+  const todoText = todo.text;
   
   todoLi.className = 'toDo';
   todoLi.innerHTML = `
@@ -40,7 +46,7 @@ function createTodoItem(todotext, todoIndex){
     <label for="${todoId}" class="customCheckbox">
       <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
     </label>
-    <label for="${todoId}" class="toDoDesc">${todotext}</label>
+    <label for="${todoId}" class="toDoDesc">${todoText}</label>
     <button class="deleteButton">
       <svg fill="var(--accentColor)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
     </button>
@@ -51,6 +57,12 @@ function createTodoItem(todotext, todoIndex){
     deleteTodoItem(todoIndex);
   })
 
+  const checkbox = todoLi.querySelector("input");
+  checkbox.addEventListener("change", () => {
+    allTodo[todoIndex].completed = checkbox.checked;
+    saveTodos();
+  })
+  checkbox.checked = todo.completed;
   return todoLi;
 }
 
